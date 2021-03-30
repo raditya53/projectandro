c9b24c94199676a8903f3063bd9c34939b4b9b57
Raditya <radityaarya53@gmail.com>
2021-03-30 13:52:39 +0700

    layout & design

--- app/src/main/java/com/example/fooddelivery/Activity_History.java
+++ app/src/main/java/com/example/fooddelivery/Activity_History.java
@@ -8,20 +8,19 @@ import android.widget.ImageButton;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class UpdateAccount extends AppCompatActivity {
-ImageButton imageButton;
+public class Activity_History  extends AppCompatActivity {
+    ImageButton back;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_update_account);
-        imageButton = findViewById(R.id.back);
-        imageButton.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_history);
+        back = findViewById(R.id.backward);
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(UpdateAccount.this, NavigasiTab.class);
+                Intent intent = new Intent(Activity_History.this, NavigasiTab.class);
                 startActivity(intent);
             }
         });
-
     }
 }
